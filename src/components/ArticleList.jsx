7e07c38c886b85206dcc92c120aeffeb1fd3af0b
Phone Sett Paing <mr.phonesettpaing@gmail.com>
2025-05-23 10:48:31 +0100

    Finsih Task 10 View Topics and it's Related articles

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,26 +2,46 @@ import { useState, useEffect } from "react";
 import ArticleCard from "./ArticleCard";
 import ArticlesFilterBar from "./ArticlesFilterBar";
 import { getArticles } from "../../api";
+import { useSearchParams } from "react-router";
 
 function ArticleList() {
   const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [error, setError] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryTopic = searchParams.get("topic");
+  console.log(queryTopic);
 
   useEffect(() => {
-    setIsLoading(true);
+    if (queryTopic !== null || queryTopic !== undefined) {
+      setIsLoading("topic");
+      setError(false);
+      const queryParams = { topic: queryTopic };
+      getArticles(queryParams)
+        .then((res) => {
+          setArticles(res);
+        })
+        .catch((err) => {
+          setError(true);
+        })
+        .finally(() => setIsLoading(null));
+    } else {
+      setIsLoading("articles");
+      setError(false);
+      getArticles()
+        .then((res) => {
+          setArticles(res);
+        })
+        .catch((err) => {
+          setError(true);
+        })
+        .finally(() => setIsLoading(null));
+    }
+  }, [queryTopic]);
 
-    getArticles()
-      .then((res) => {
-        setArticles(res);
-      })
-      .catch((err) => {
-        setError(true);
-      })
-      .finally(() => setIsLoading(false));
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading === "topic")
+    return <p>Fetching Articles Related to Topic...</p>;
+  if (isLoading === "articles") return <p>Fetching all Articles...</p>;
   if (error) return <p>Sorry, something went wrong there</p>;
 
   return (
